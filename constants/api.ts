e3edda53b9e221fec799d7e6c07527581d3c211b
wmcheung <wesleylgr33@gmail.com>
2024-10-06 23:50:43 +0200

    chore: add constants

---
+++ constants/api.ts
@@ -0,0 +1,1 @@
+export const BASE_URL = "https://work-test-web-2024-eze6j4scpq-lz.a.run.app";
---
+++ constants/delivery-times.ts
@@ -0,0 +1,6 @@
+export const DELIVERY_TIMES = [
+  { label: "0-10 min", range: [0, 10] },
+  { label: "10-30 min", range: [10, 30] },
+  { label: "30-60 min", range: [30, 60] },
+  { label: "1 hour+", range: [60, 1000] },
+];
